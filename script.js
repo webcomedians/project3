a20ee5fe4606d76c7ad86f09da69d86ec9a845a7
Connotating <figureyou@gmail.com>
2016-11-29 19:27:40 -0600

    Boom all done, lets get a live API and re initialize this bad boy!

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
-function getJSON(url, success) {
+	var SportsAPI = 'https://www.mysportsfeeds.com/api/feed/sample/pull/nba/2015-2016-regular/playoff_team_standings.json?';
+
+  $(document).ready(function() {
+  console.log("once clicked");
+	$.getJSON(SportsAPI, function(json){
+		console.log("It worked");
+			
+
+	
+  
+   
+	$.each(json.playoffteamstandings.conference, function(gameNum, obj){
+		$.each(obj.teamentry, function(i,obj) {
+			console.log(obj.rank);
+			$("#data").append("<li>" + obj.team.City + " " + obj.team.Name + ", " + obj.rank+ "</li>");
+
+		});
+		
+        });
+      });
+});
+
+
+// I KID YOU NOT THE SCRIPT TAG WAS WRONG AND DIDNT LOAD IN JQUERY 
+//I HAVE THE STALEST FACE 11/29/2016
+	
+
+//Too much recursion error now being displayed will revisit later but it runs
+/*Old code: function getJSON(url, success) {
 
     var ud = '_' + +new Date,
         script = document.createElement('script'),
@@ -14,18 +42,14 @@ function getJSON(url, success) {
     head.appendChild(script);
 
 }
-
-getJSON('https://www.mysportsfeeds.com/api/feed/sample/pull/nba/2015-2016-regular/full_game_schedule.json?', function(data){
-    console.log(data);
+$(document).ready(function(){
+getJSON('https://www.mysportsfeeds.com/api/feed/sample/pull/nba/2015-2016-regular/full_game_schedule.json?'), function(data){
+    JSON.parse(data);
+	$.each(data.fullgameschedule.gameentry, function(gameNum,obj){
+		console.log(obj.date + " " + obj.awayTeam);
+	})
 });  
+});*/
 
-//The comments below are overstack code for working with objects in the
 
-//jSON file 
-// $.getJSON('people.json', function(data) {
-     //  $.each(data.person, function(i, f) {
-       //   var tblRow = "<tr>" + "<td>" + f.firstName + "</td>" +
-         //  "<td>" + f.lastName + "</td>" + "<td>" + f.job + "</td>" + //
-//"<td>" + f.roll + "</td>" + "</tr>"
-  //         $(tblRow).appendTo("#userdata tbody");
-    // }); >
+
