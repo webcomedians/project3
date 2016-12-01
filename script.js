311732973c0dc9bfbff6a970a2d97658f96b7cd2
kiskisiit1131 <kiskisakvile@gmail.com>
2016-12-01 14:07:32 -0600

    working on getting API live (#11)
    * added my info portion
    
    * removed pound signs because of oops
    
    * changed up format so easier to read
    
    * added html file
    
    * added the barebones to it
    
    * tried to fix conflicts
    
    * tried to fix conflicts
    
    * added nba logo for page
    
    * Create test.html
    
    * Delete test.html
    
    * Create test.html
    
    * moved to pictures folder
    
    * added in header to page and nba logo
    
    * added informative content to the page
    
    * added footer to html
    
    * adding no conflict
    
    * adjusted site info and javascript aspects
    
    * adjusted so you can see each conference standing
    
    * made progress on button, will continue to work on it tomorrow
    
    * added id's for button
    
    * updating button, working on getting API live
    
    * updating button, working on getting API live

--- script.js
+++ script.js
@@ -2,14 +2,13 @@ $.noConflict();
 (function($){
 $(document).ready(
 function getJSON(url, success) {
-	var SportsAPI = 'https://www.mysportsfeeds.com/api/feed/sample/pull/nba/2016-2017 Regular/playoff_team_standings.json?';
-
+	var SportsAPI = 'https://www.mysportsfeeds.com/api/feed/sample/pull/nba/2015-2016-regular/playoff_team_standings.json?';
   $(document).ready(function() {
   console.log("once clicked");
 	$.getJSON(SportsAPI, function(json){
 		console.log("It worked");
 
-    var cityName, teamName, wins, losses, response;
+    var cityName, teamName;
 
    for(var i = 0; i < 15; i++){
     cityName = json.playoffteamstandings.conference[0].teamentry[i].team.City;
@@ -24,36 +23,18 @@ function getJSON(url, success) {
     
 	$.each(json.playoffteamstandings.conference, function(gameNum, obj){
 		$.each(obj.teamentry, function(i,obj) {
-			console.log(json.playoffteamstandings.conference.teamentry);
         
    
-   $('#winloss-form').on('submit', function(event) {
+   $('#division-form').on('submit', function(event) {
      console.log("does this work");
-    var query = $('#team').val();
     $.get(
-<<<<<<< HEAD
       'https://www.mysportsfeeds.com/api/feed/pull/nba/2016-2017%20Regular/division_team_standings.json?teamstats=' + query,
-=======
-      'https://www.mysportsfeeds.com/api/feed/sample/pull/nba/2015-2016-regular/playoff_team_standings.json?' + query,
->>>>>>> 2fc6d705c051697dd2925ec4554096984511036d
       function(data) {
-        response = ($("#team").val());
-        for(var i = 0; i < 30; i++){
-          console.log("test");
-          if (json.playoffteamstandings.conference.teamentry[i].team.Name == response){ // need to adjust because considered undefined
-            wins = json.playoffteamstandings.conference.teamentry[i].stats.Wins; // will adjust 
-            losses = json.playoffteamstandings.conference.teamentry[i].stats.Losses; // will adjust
-              $('#winloss').append( "<p>Wins: " + wins + "Losses: " + losses + "</p>"); 
-          }
-          else {
-          $('#winloss').append("<p>Make sure you entered in the team name correctly.</p>");  
-          }
-        }
+
+
       });  
   
       });
-<<<<<<< HEAD
-=======
 
 });
 });
@@ -61,14 +42,7 @@ function getJSON(url, success) {
 });
 });
 })(jQuery); 
->>>>>>> 2fc6d705c051697dd2925ec4554096984511036d
 
-});
-});
-});
-});
-});
-})(jQuery); 
 
 /*
 // I KID YOU NOT THE SCRIPT TAG WAS WRONG AND DIDNT LOAD IN JQUERY 
